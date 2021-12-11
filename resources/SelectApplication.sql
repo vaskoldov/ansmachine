555058c13c10835c8a45c01525be2123b4796ca2
VAskoldov <vaskoldov@hemulen-it.ru>
2021-12-11 08:20:31 +0300

    Исправлен SQL-скрипт для выборки заявлений

--- resources/SelectApplication.sql
+++ resources/SelectApplication.sql
@@ -2,7 +2,9 @@ SELECT req.id, req.message_id, req.destination_name, req.inquiry_version, cnt.co
 FROM core.message_metadata req
     LEFT JOIN core.message_content cnt ON req.id = cnt.id
     LEFT JOIN core.message_metadata resp ON resp.reference_id = req.id
+    LEFT JOIN core.message_state ms ON ms.id = req.id
 WHERE req.message_type = 'REQUEST'
+  AND ms.transport_direct = 'INCOMING'
   AND req.inquiry_version IS NOT NULL
   AND resp.id IS NULL
 ORDER BY req.delivery_date;
